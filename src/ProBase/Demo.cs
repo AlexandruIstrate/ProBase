1ab7bca7ccf54eeaf6f434fcf1bf86c77eb3ec6c
AlexandruIstrate <38143225+AlexandruIstrate@users.noreply.github.com>
2019-09-06 17:56:37 +0300

    Configured NuGet

---
+++ src/ProBase/Demo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProBase
+{
+    public class Demo
+    {
+        public void Validate()
+        {
+            Console.WriteLine("This project works!");
+        }
+    }
+}
