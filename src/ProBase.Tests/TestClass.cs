4e9fa79829e9f0c6a88bd67ead1fb3944d1bc4c3
AlexandruIstrate <38143225+AlexandruIstrate@users.noreply.github.com>
2019-09-06 17:44:23 +0300

    Added NUnit tests to solution

---
+++ src/ProBase.Tests/TestClass.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace ProBase.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual(2, 2, "These two numbers should always be equal");
+        }
+    }
+}
