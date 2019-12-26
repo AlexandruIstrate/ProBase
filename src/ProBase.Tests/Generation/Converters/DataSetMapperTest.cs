c3e25ccd6a4ad2a70b9d2726dc2e54dff6f2bd5b
AlexandruIstrate <38143225+AlexandruIstrate@users.noreply.github.com>
2019-12-26 23:57:59 +0200

    Added test stubs

---
+++ src/ProBase.Tests/Generation/Converters/DataSetMapperTest.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using ProBase.Generation.Converters;
+using System;
+using System.Data;
+using System.Text;
+
+namespace ProBase.Tests.Generation.Converters
+{
+    [Ignore("Not finished")]
+    [TestFixture]
+    public class DataSetMapperTest
+    {
+        [Test]
+        public void CanMapDataRow()
+        {
+            DataRow dataRow = CreateDataRow();
+
+            Assert.DoesNotThrow(() =>
+            {
+                dataSetMapper.Map<StringBuilder>(dataRow);
+            },
+            "The map operation must be successful");
+        }
+
+        [Test]
+        public void CanMapDataTable()
+        {
+            DataTable dataTable = CreateDataTable();
+
+            Assert.DoesNotThrow(() =>
+            {
+                dataSetMapper.Map<StringBuilder>(dataTable);
+            },
+            "The map operation must be successful");
+        }
+
+        private DataRow CreateDataRow()
+        {
+            throw new NotImplementedException();
+        }
+
+        private DataTable CreateDataTable()
+        {
+            throw new NotImplementedException();
+        }
+
+        private DataSetMapper dataSetMapper;
+    }
+}
---
+++ src/ProBase.Tests/Generation/Converters/PropertyMapperTest.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace ProBase.Tests.Generation.Converters
+{
+    [Ignore("Not finished")]
+    [TestFixture]
+    public class PropertyMapperTest
+    {
+    }
+}
