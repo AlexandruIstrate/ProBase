136baf74dacc2be8a4affe97e9554fde45db54e2
AlexandruIstrate <38143225+AlexandruIstrate@users.noreply.github.com>
2020-02-01 23:55:49 +0200

    Configured the async procedure call

--- src/ProBase/Generation/Call/AsyncProcedureCall.cs
+++ src/ProBase/Generation/Call/AsyncProcedureCall.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using ProBase.Data;
+using ProBase.Utils;
+using System;
+using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Reflection.Emit;
 
 namespace ProBase.Generation.Call
@@ -16,11 +20,39 @@ namespace ProBase.Generation.Call
                 Type taskType = resultType.GetGenericArguments().First();
 
                 // Call the mapper method
+                generator.Emit(OpCodes.Callvirt, GetResultTaskMethod(taskType));
             }
             else
             {
                 // We have a simple Task
+
+                // In this case, execute an async non-query. The result type will be of type
+                // Task<int> but we can use it as a plain Task since Task<T> inherrits from Task.
+                generator.Emit(OpCodes.Callvirt, GetNonQuerryMethod());
             }
+        }
+
+        private MethodInfo GetResultTaskMethod(Type returnType)
+        {
+            // For an int, we have an non-query call
+            if (returnType == typeof(int))
+            {
+                return GetNonQuerryMethod();
+            }
+
+            // For a DataSet, we have an unmapped scalar call
+            if (returnType == typeof(DataSet))
+            {
+                return ClassUtils.GetMethod<IProcedureMapper>(nameof(IProcedureMapper.ExecuteScalarProcedureAsync));
+            }
+
+            // If we have any other type, then it must be mapped
+            return ClassUtils.GetMethod<IProcedureMapper>(nameof(IProcedureMapper.ExecuteMappedProcedureAsync));
+        }
+
+        private MethodInfo GetNonQuerryMethod()
+        {
+            return ClassUtils.GetMethod<IProcedureMapper>(nameof(IProcedureMapper.ExecuteNonQueryProcedureAsync));
         }
     }
 }
