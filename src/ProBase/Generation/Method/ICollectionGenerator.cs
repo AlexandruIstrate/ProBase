ec14fac5ec6457883aa59d87290ae383e71d2286
AlexandruIstrate <38143225+AlexandruIstrate@users.noreply.github.com>
2020-03-05 23:35:52 +0200

    Added missing documentation

--- src/ProBase/Generation/Method/ICollectionGenerator.cs
+++ src/ProBase/Generation/Method/ICollectionGenerator.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using ProBase.Utils;
 using System.Reflection;
 using System.Reflection.Emit;
 
@@ -15,35 +15,7 @@ namespace ProBase.Generation.Method
         /// <param name="parameters">The parameters passed into the method</param>
         /// <param name="fields">The fields of the generated class</param>
         /// <param name="generator">The generator to use for generating MSIL instructions</param>
-        /// <returns>The local builder associated with the array</returns>
+        /// <returns>A parameter collection</returns>
         ParameterCollection Generate(ParameterInfo[] parameters, FieldInfo[] fields, ILGenerator generator);
-    }
-
-    internal class ParameterCollection
-    {
-        public LocalBuilder CollectionLocal { get; set; }
-
-        public LocalBuilder this[ParameterInfo index]
-        {
-            get => GetBuilderForParameter(index);
-            set => SetBuilderForParameter(index, value);
-        }
-
-        public LocalBuilder GetBuilderForParameter(ParameterInfo parameterInfo)
-        {
-            if (!parameterDictionary.TryGetValue(parameterInfo, out LocalBuilder value))
-            {
-                return null;
-            }
-
-            return value;
-        }
-
-        public void SetBuilderForParameter(ParameterInfo parameter, LocalBuilder builder)
-        {
-            parameterDictionary[parameter] = builder;
-        }
-
-        private readonly Dictionary<ParameterInfo, LocalBuilder> parameterDictionary = new Dictionary<ParameterInfo, LocalBuilder>();
     }
 }
